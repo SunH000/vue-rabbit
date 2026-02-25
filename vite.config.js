6b7715c3c96caf609be66f5a09b801a0f4f6daa3
SunH000 <176661177@qq.com>
2026-02-25 20:48:17 +0800

    elementUI主题色 更改

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' }),
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2. 自动导入定制化样式文件进行样式覆盖
+        // @use "@/styles/var.scss" as *;
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 })
